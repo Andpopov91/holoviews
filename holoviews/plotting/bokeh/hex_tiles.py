7c7fd4032adf95ed9818c46ff039a1cd8066e3b1
Andrey Popov <49846482+Andpopov91@users.noreply.github.com>
2023-08-20 19:51:01 +0100

    Update hex_tiles.py

--- holoviews/plotting/bokeh/hex_tiles.py
+++ holoviews/plotting/bokeh/hex_tiles.py
@@ -92,11 +92,13 @@ class hex_binning(Operation):
         df_temp.reset_index(inplace = True)
 
         print('columns:' ,df_temp)
-        
+
         # Below is a bit changed to count unique 'device_id_time' in the given square
         # df_temp_grouped = df_temp.groupby(by = ['longitude' , 'latitude']).count() #['longitude' , 'latitude'] should be kdims variable, but currently kdims has a type which I am not sure how to convert to a list
         df_temp_grouped = df_temp.groupby(by = ['longitude' , 'latitude'])['device_id_time'].nunique() #['longitude' , 'latitude'] should be kdims variable, but currently kdims has a type which I am not sure how to convert to a list
 
+        print('grouped df is:')
+        print(df_temp_grouped)
         df_temp_grouped_filtered = df_temp_grouped[(df_temp_grouped > self.p.min_count).any(1)]
         
         data = pd.merge(left = df_temp,
